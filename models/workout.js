5f0ac04bbb2ff81b113872b7293ad9e88378c838
Anthony Arroyo <anthony.aarroy2@gmail.com>
2020-05-04 02:50:00 -0500

    fixed schema

--- models/workout.js
+++ models/workout.js
@@ -39,19 +39,6 @@ const workoutSchema = new Schema({
 
       }
       ]
-},
-{
-  toJSON: {
-    virtuals: true
-  }
-}
-);
-
-workoutSchema.virtual("totalDuration").get(function() {
-  // "reduce" array of exercises down to just the sum of their durations
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercise.duration;
-  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
